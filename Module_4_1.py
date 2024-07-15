789debac209296eeae900afeb69a5412eb18a923
il-svetlana <rtk-rk@mail.ru>
2024-07-16 01:01:20 +0400

    Add files via upload

---
+++ Module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+print(fake_divide(20,10))
+print(fake_divide(5,0))
+print(true_divide(20,10))
+print(true_divide(5,0))
