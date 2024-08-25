09d325b18123315cb77438662e55e95f71a8fccb
il-svetlana <rtk-rk@mail.ru>
2024-08-25 13:31:38 +0400

    Add files via upload

---
+++ Задание_5-03_Множественное наследование v2.py
@@ -0,0 +1,42 @@
+class Horse:
+    sound = 'Frrr'
+
+    def __init__(self, x_distance=0):
+        self.x_distance = x_distance
+
+    def run(self, dx):
+        self.x_distance += dx
+
+class Eagle:
+    sound = 'I train, eat, sleep, and repeat'
+
+    def __init__(self, y_distance=0):
+        self.y_distance = y_distance
+
+    def fly(self, dy):
+        self.y_distance += dy
+
+class Pegasus(Horse, Eagle):
+
+    def __init__(self, x_distance=0, y_distance=0):
+        self.x_distance = x_distance
+        self.y_distance = y_distance
+
+    def move(self, dx, dy):
+        super().run(dx)
+        super().fly(dy)
+
+    def get_pos(self):
+        position = (self.x_distance, self.y_distance)
+        return position
+
+    def voice(self):
+        sound = Horse.sound + ' ' + Eagle.sound
+        return sound
+
+desert = Pegasus()
+desert.move(10, 3)
+print('Координаты пегаса: ', desert.get_pos())
+print('Пегас издает звуки: ', desert.voice())
+
+
